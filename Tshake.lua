bc28f6c2a9b4f577204bd7a4a2a2ece996cedba1
TshAkETEAM <52387368+TshAkETEAM@users.noreply.github.com>
2019-07-10 01:08:03 +0300

    Update Tshake.lua

--- Tshake.lua
+++ Tshake.lua
@@ -4585,23 +4585,6 @@ send(msg.chat_id_, msg.id_, 1, texts, 1, 'md')
 end
 resolve_username(ap[2],id_by_username)
 end  
-if text:match("^جلب صوره (%d+)$") and msg.reply_to_message_id_ == 0 and not database:get('tshake:'..bot_id..'get:photo'..msg.chat_id_) then
-local pronumb = {string.match(text, "^(جلب صوره) (%d+)$")}
-local ph = pronumb[2] - 1
-local function gpro(extra, result, success)
-if result.photos_[ph] then
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[ph].sizes_[1].photo_.persistent_id_)
-else
-send(msg.chat_id_, msg.id_, 1, "❕┇لا تملك صوره رقم <b>{"..pronumb[2].."}</b> في حسابك", 1, 'html')
-end
-end
-tdcli_function ({
-ID = "GetUserProfilePhotos",
-user_id_ = msg.sender_user_id_,
-offset_ = 0,
-limit_ = pronumb[2]
-}, gpro, nil)
-end
 if text:match("^وضع تكرار (%d+)$") and (is_owner(msg) or is_creatorbasic(msg)) then
 local floodmax = {string.match(text, "^(وضع تكرار) (%d+)$")}
 if tonumber(floodmax[2]) < 2 then
@@ -6012,22 +5995,6 @@ send(msg.chat_id_, msg.id_, 1, '📸 ※ بلفعل الايدي بالصوره 
 else
 send(msg.chat_id_, msg.id_, 1, '📸 ※ تم تعطيل  الايدي بالصوره ✓', 1, 'md')
 database:set('tshake:'..bot_id..'id:photo'..msg.chat_id_,true)
-end
-end
-if (text and text == 'تفعيل جلب صوره') and (is_owner(msg) or is_creatorbasic(msg)) then
-if not database:get('tshake:'..bot_id..'get:photo'..msg.chat_id_) then
-send(msg.chat_id_, msg.id_, 1, '☑┇جلب صوره بالفعل تم تفعيله', 1, 'md')
-else
-send(msg.chat_id_, msg.id_, 1, '☑┇تم تفعيل جلب صوره', 1, 'md')
-database:del('tshake:'..bot_id..'get:photo'..msg.chat_id_)
-end
-end
-if (text and text == 'تعطيل جلب صوره') and (is_owner(msg) or is_creatorbasic(msg)) then
-if database:get('tshake:'..bot_id..'get:photo'..msg.chat_id_) then
-send(msg.chat_id_, msg.id_, 1, '☑┇جلب صوره بالفعل تم تعطيله', 1, 'md')
-else
-send(msg.chat_id_, msg.id_, 1, '☑┇تم تعطيل جلب صوره', 1, 'md')
-database:set('tshake:'..bot_id..'get:photo'..msg.chat_id_,true)
 end
 end
 if  (text and text == 'تفعيل وضع المدفوع') and tonumber(msg.sender_user_id_) == tonumber(sudo_add) then
