ef1f40f5c604ae4ae57b7b22de3f0d27e279f9f1
TshAkETEAM <52387368+TshAkETEAM@users.noreply.github.com>
2019-07-31 18:01:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -220,7 +220,6 @@ echo -e "\e[38;5;77m       »»     Install Tshake [91%]     ««\e[0m"
 cd ..
 cd Tshake
 rm -rf "luarocks-2.2.2"
-wget "http://tshake.gq/tg"
 apt install redis-server -y
 reset
 echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
